cbd63da02590a0a963ad80f97ec0fad32c437dee
vsihvola <ville.sihvola8@gmail.com>
2020-08-24 22:56:51 +0300

    changes

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
-function getData() {
-    const csvFilePath = './accounts1.csv' // Resource.csv in your case
-    const csv = require('csvtojson') //. Make sure you have this line in order to call functions from this modules
-    var i = 1;
-    csv()
-        .fromFile(csvFilePath)
-        .then((jsonObj) => {
+const Faceit = require("faceit-js");
+const api = new Faceit(`cb0281a0-8c84-4d16-9018-e305ac8986a2`);
 
-            var i = 0;
-            //var https = 'https://www.faceit.com/fi/players/';
-            jsonObj.forEach(obj =>
-                .renameKey(obj, ';', 'id')
-                .removeEverythingAfterSpace(obj));
+//const steamSite = "https://steamcommunity.com/profiles/";
 
-            // jsonObj.forEach(obj => 
-            console.log(jsonObj);
-            //console.log(https + jsonObj[1].id + '/');
 
-        })
+async function getPlayers() {
+
+    /*
+    Palauttaa top100 
+    player_id: '9f8d35f7-8773-442c-b68f-41c31e318ddc',
+    nickname: 'Syn-G-',
+    country: 'fi',
+    position: 99,
+    faceit_elo: 3156,
+    game_skill_level: 10
+    */
+    const players = await getTopPlayers(100)
+
+
+    /*
+    Palauttaa datan profiilista
+    friends_ids: [..]
+    bans: [],
+    new_steam_id: '[U:1:26565773]',
+    steam_id_64: '76561197986831501',
+    steam_nickname: 'xartE',
+    membership_type: 'premium',
+    memberships: [ 'premium' ],
+    faceit_url: 'https://www.faceit.com/{lang}/players/xartE'
+    */
+
+    const theProfile = await getProfileData(players.items[1]);
+
+
+    console.log(theProfile);
+
 }
 
-function renameKey(obj, oldKey, newKey) {
-    obj[newKey] = obj[oldKey];
-    delete obj[oldKey];
-
+async function getTopPlayers(limit) {
+    return await api
+        .rankings("csgo", "EU", "fi", limit)
+        .catch(err =>
+            console.log(err))
 }
 
-function removeEverythingAfterSpace(obj) {
-    let i = obj.id.length;
-    obj.id = obj.id.slice(0, i - 2);
-
+async function getProfileData(player) {
+    return await api
+        .players(player.player_id)
+        .catch(err =>
+            console.log(err))
 }
 
-function addRankAndWebsite(obj) {
-    obj['rank'] = '1';
-}
-
-
-
-getData();
+getPlayers();
