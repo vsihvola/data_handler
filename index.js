763bb7a413498915e8bfb0238c6cad654af15e9a
vsihvola <ville.sihvola8@gmail.com>
2020-08-24 23:31:54 +0300

    get steam

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Faceit = require("faceit-js");
 const api = new Faceit(`cb0281a0-8c84-4d16-9018-e305ac8986a2`);
 
-//const steamSite = "https://steamcommunity.com/profiles/";
+const steamSite = "https://steamcommunity.com/profiles/";
 
+const data = [{}];
 
 async function getPlayers() {
 
     /*
-    Palauttaa top100 
+    Palauttaa top100 suomen pelaajat
+
     player_id: '9f8d35f7-8773-442c-b68f-41c31e318ddc',
     nickname: 'Syn-G-',
     country: 'fi',
@@ -15,8 +17,11 @@ async function getPlayers() {
     faceit_elo: 3156,
     game_skill_level: 10
     */
-    const players = await getTopPlayers(100)
+    var x = 0;
+    var y = 0;
 
+
+    const players = await getTopPlayers(0, 100)
 
     /*
     Palauttaa datan profiilista
@@ -30,16 +35,25 @@ async function getPlayers() {
     faceit_url: 'https://www.faceit.com/{lang}/players/xartE'
     */
 
-    const theProfile = await getProfileData(players.items[1]);
+    const allPlayersData = await Promise.all(players.items.map((item) => {
+        return getProfileData(item);
+    }));
 
+    allPlayersData.forEach(temp => {
+        data.push({
+            ranking: x = x + 1,
+            nickname: temp.nickname,
+            steam: steamSite + temp.steam_id_64
+        })
+    })
 
-    console.log(theProfile);
+    console.log(data);
 
 }
 
-async function getTopPlayers(limit) {
+async function getTopPlayers(offset, limit) {
     return await api
-        .rankings("csgo", "EU", "fi", limit)
+        .rankings("csgo", "EU", "fi", offset, limit)
         .catch(err =>
             console.log(err))
 }
